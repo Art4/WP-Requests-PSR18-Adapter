ce1454b420f341b9bcb8927486e0b4172a5033c0
Art4 <art4@wlabs.de>
2026-03-24 05:39:11 +0100

    test: add tests for tell()

--- tests/Psr/StringBasedStream/TellTest.php
+++ tests/Psr/StringBasedStream/TellTest.php
@@ -14,10 +14,54 @@ final class TellTest extends TestCase
      *
      * @covers \Art4\Requests\Psr\StringBasedStream::tell
      */
-    public function testTellReturnsZeroOnFirstCall(): void
+    public function testTellOnFirstCallReturnsZero(): void
     {
         $stream = StringBasedStream::createFromString('0123456789');
 
         TestCase::assertSame(0, $stream->tell());
+    }
+
+    /**
+     * Tests receiving an integer when using tell() method.
+     *
+     * @covers \Art4\Requests\Psr\StringBasedStream::tell
+     */
+    public function testTellAfterSeekWithSeekSetReturnsInteger(): void
+    {
+        $stream = StringBasedStream::createFromString('0123456789');
+
+        $stream->seek(3, SEEK_SET);
+
+        TestCase::assertSame(3, $stream->tell());
+    }
+
+    /**
+     * Tests receiving an integer when using tell() method.
+     *
+     * @covers \Art4\Requests\Psr\StringBasedStream::tell
+     */
+    public function testTellAfterSeekWithSeekCurReturnsInteger(): void
+    {
+        $stream = StringBasedStream::createFromString('0123456789');
+
+        $stream->seek(5, SEEK_SET);
+        $stream->seek(3, SEEK_CUR);
+
+        TestCase::assertSame(8, $stream->tell());
+    }
+
+    /**
+     * Tests receiving an integer when using tell() method.
+     *
+     * @covers \Art4\Requests\Psr\StringBasedStream::tell
+     */
+    public function testTellAfterSeekWithSeekEndReturnsInteger(): void
+    {
+        $stream = StringBasedStream::createFromString('0123456789');
+
+        $stream->seek(5, SEEK_SET);
+        $stream->seek(0, SEEK_END);
+
+        TestCase::assertSame(10, $stream->tell());
     }
 }
