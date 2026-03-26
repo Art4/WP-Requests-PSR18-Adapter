60c1b87d0eac7f6d087ea22cd280ee392a0f7f3d
Art4 <art4@wlabs.de>
2026-03-26 06:20:42 +0100

    feat: implement getMetadata()

--- src/Psr/StringBasedStream.php
+++ src/Psr/StringBasedStream.php
@@ -276,17 +276,23 @@ final class StringBasedStream implements StreamInterface
      * stream_get_meta_data() function.
      *
      * @link http://php.net/manual/en/function.stream-get-meta-data.php
-     * @param string $key Specific metadata to retrieve.
+     * @param string|null $key Specific metadata to retrieve.
      * @return array|mixed|null Returns an associative array if no key is
      *     provided. Returns a specific key value if a key is provided and the
      *     value is found, or null if the key is not found.
      */
     public function getMetadata(?string $key = null)
     {
-        if (is_string($key)) {
-            return null;
+        $meta = [
+            'seekable' => $this->seekable,
+            'readable' => true,
+            'writable' => false,
+        ];
+
+        if ($key === null) {
+            return $meta;
         }
 
-        return [];
+        return $meta[$key] ?? null;
     }
 }
--- tests/Psr/StringBasedStream/GetMetadataTest.php
+++ tests/Psr/StringBasedStream/GetMetadataTest.php
@@ -18,7 +18,14 @@ final class GetMetadataTest extends TestCase
     {
         $stream = StringBasedStream::createFromString('');
 
-        TestCase::assertSame([], $stream->getMetadata());
+        TestCase::assertSame(
+            [
+                'seekable' => true,
+                'readable' => true,
+                'writable' => false,
+            ],
+            $stream->getMetadata()
+        );
     }
 
     /**
@@ -26,7 +33,34 @@ final class GetMetadataTest extends TestCase
      *
      * @covers \Art4\Requests\Psr\StringBasedStream::getMetadata
      */
-    public function testGetMetadataWithKeyReturnsNull(): void
+    public function testGetMetadataWithKeysReturnsValues(): void
+    {
+        $stream = StringBasedStream::createFromString('');
+
+        TestCase::assertTrue($stream->getMetadata('seekable'));
+        TestCase::assertTrue($stream->getMetadata('readable'));
+        TestCase::assertFalse($stream->getMetadata('writable'));
+    }
+
+    /**
+     * Tests receiving null when using getMetadata().
+     *
+     * @covers \Art4\Requests\Psr\StringBasedStream::getMetadata
+     */
+    public function testGetMetadataWithSeekableKeyAfterCloseReturnsFalse(): void
+    {
+        $stream = StringBasedStream::createFromString('');
+        $stream->close();
+
+        TestCase::assertFalse($stream->getMetadata('seekable'));
+    }
+
+    /**
+     * Tests receiving null when using getMetadata().
+     *
+     * @covers \Art4\Requests\Psr\StringBasedStream::getMetadata
+     */
+    public function testGetMetadataWithUnknownKeyReturnsNull(): void
     {
         $stream = StringBasedStream::createFromString('');
 
